a341127c4eca4e8c0b721a70ccd5d422456337fb
Ivan Ermolaev <ivermolaev@gmail.com>
2013-06-12 17:30:25 +0300

    Fixed client's project file

--- client/project.clj
+++ client/project.clj
@@ -3,5 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.5.0"]
+                 [seesaw "1.4.3"]
+                 [cheshire "5.0.2"]
+                 [org.blancas/kern "0.7.0"]]
   :main client.core)
